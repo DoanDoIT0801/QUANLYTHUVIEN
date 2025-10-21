cdf2be0d29ebf89d64ffe1c3c80f479c6ad8f1ea
no0613332 <no0613332@gmail.com>
2025-10-21 14:47:21 +0700

    frmNhanvien

--- DOANNHOM/frnQlNhanVien.Designer.cs
+++ DOANNHOM/frnQlNhanVien.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DOANNHOM
 {
-    partial class frnQlNhanVien
+    partial class frmQLNhanVien
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,323 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dgvNV = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnDK = new System.Windows.Forms.Button();
+            this.btnDoiMK = new System.Windows.Forms.Button();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rdNu = new System.Windows.Forms.RadioButton();
+            this.rdNam = new System.Windows.Forms.RadioButton();
+            this.txtTenNV = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnChonAnh = new System.Windows.Forms.Button();
+            this.picNV = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNV)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picNV)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(1005, 511);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 31);
+            this.btnThoat.TabIndex = 9;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dgvNV);
+            this.panel3.Location = new System.Drawing.Point(402, 71);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(684, 439);
+            this.panel3.TabIndex = 8;
+            // 
+            // dgvNV
+            // 
+            this.dgvNV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNV.Location = new System.Drawing.Point(3, 3);
+            this.dgvNV.Name = "dgvNV";
+            this.dgvNV.RowHeadersWidth = 51;
+            this.dgvNV.RowTemplate.Height = 24;
+            this.dgvNV.Size = new System.Drawing.Size(675, 431);
+            this.dgvNV.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnXoa);
+            this.panel2.Controls.Add(this.btnDK);
+            this.panel2.Controls.Add(this.btnDoiMK);
+            this.panel2.Controls.Add(this.txtMK);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.txtDiaChi);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.txtSDT);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.rdNu);
+            this.panel2.Controls.Add(this.rdNam);
+            this.panel2.Controls.Add(this.txtTenNV);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtMaNV);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(8, 71);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(388, 439);
+            this.panel2.TabIndex = 7;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(271, 382);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(105, 30);
+            this.btnXoa.TabIndex = 15;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnDK
+            // 
+            this.btnDK.Location = new System.Drawing.Point(139, 382);
+            this.btnDK.Name = "btnDK";
+            this.btnDK.Size = new System.Drawing.Size(117, 30);
+            this.btnDK.TabIndex = 14;
+            this.btnDK.Text = "Đăng Ký";
+            this.btnDK.UseVisualStyleBackColor = true;
+            this.btnDK.Click += new System.EventHandler(this.btnDK_Click);
+            // 
+            // btnDoiMK
+            // 
+            this.btnDoiMK.Location = new System.Drawing.Point(6, 382);
+            this.btnDoiMK.Name = "btnDoiMK";
+            this.btnDoiMK.Size = new System.Drawing.Size(116, 30);
+            this.btnDoiMK.TabIndex = 13;
+            this.btnDoiMK.Text = "Đổi Mật Khẩu";
+            this.btnDoiMK.UseVisualStyleBackColor = true;
+            this.btnDoiMK.Click += new System.EventHandler(this.btnDoiMK_Click);
+            // 
+            // txtMK
+            // 
+            this.txtMK.Location = new System.Drawing.Point(156, 310);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(220, 22);
+            this.txtMK.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(14, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Mật Khẩu";
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(156, 261);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(220, 22);
+            this.txtDiaChi.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Địa Chỉ";
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(241, 94);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(135, 22);
+            this.txtSDT.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(152, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SĐT";
+            // 
+            // rdNu
+            // 
+            this.rdNu.AutoSize = true;
+            this.rdNu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdNu.Location = new System.Drawing.Point(297, 151);
+            this.rdNu.Name = "rdNu";
+            this.rdNu.Size = new System.Drawing.Size(51, 24);
+            this.rdNu.TabIndex = 6;
+            this.rdNu.TabStop = true;
+            this.rdNu.Text = "Nữ";
+            this.rdNu.UseVisualStyleBackColor = true;
+            // 
+            // rdNam
+            // 
+            this.rdNam.AutoSize = true;
+            this.rdNam.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdNam.Location = new System.Drawing.Point(185, 151);
+            this.rdNam.Name = "rdNam";
+            this.rdNam.Size = new System.Drawing.Size(65, 24);
+            this.rdNam.TabIndex = 5;
+            this.rdNam.TabStop = true;
+            this.rdNam.Text = "Nam";
+            this.rdNam.UseVisualStyleBackColor = true;
+            // 
+            // txtTenNV
+            // 
+            this.txtTenNV.Location = new System.Drawing.Point(156, 216);
+            this.txtTenNV.Name = "txtTenNV";
+            this.txtTenNV.Size = new System.Drawing.Size(220, 22);
+            this.txtTenNV.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tên Nhân Viên";
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(241, 40);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(135, 22);
+            this.txtMaNV.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(148, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã NV-TK";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnChonAnh);
+            this.panel4.Controls.Add(this.picNV);
+            this.panel4.Location = new System.Drawing.Point(3, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(143, 195);
+            this.panel4.TabIndex = 0;
+            // 
+            // btnChonAnh
+            // 
+            this.btnChonAnh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChonAnh.Location = new System.Drawing.Point(3, 160);
+            this.btnChonAnh.Name = "btnChonAnh";
+            this.btnChonAnh.Size = new System.Drawing.Size(138, 31);
+            this.btnChonAnh.TabIndex = 1;
+            this.btnChonAnh.Text = "Chọn Ảnh";
+            this.btnChonAnh.UseVisualStyleBackColor = true;
+            // 
+            // picNV
+            // 
+            this.picNV.Location = new System.Drawing.Point(3, 3);
+            this.picNV.Name = "picNV";
+            this.picNV.Size = new System.Drawing.Size(136, 151);
+            this.picNV.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picNV.TabIndex = 0;
+            this.picNV.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1087, 44);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(412, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản Lý Nhân Viên";
+            // 
+            // frnQlNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1093, 552);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frnQlNhanVien";
             this.Text = "frnQlNhanVien";
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNV)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picNV)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dgvNV;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnDK;
+        private System.Windows.Forms.Button btnDoiMK;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rdNu;
+        private System.Windows.Forms.RadioButton rdNam;
+        private System.Windows.Forms.TextBox txtTenNV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnChonAnh;
+        private System.Windows.Forms.PictureBox picNV;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- DOANNHOM/frnQlNhanVien.cs
+++ DOANNHOM/frnQlNhanVien.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +12,205 @@ using System.Windows.Forms;
 
 namespace DOANNHOM
 {
-    public partial class frnQlNhanVien : Form
+    public partial class frmQLNhanVien : Form
     {
-        public frnQlNhanVien()
+        string connectionString = "data source=LAPTOP-9GGQMNJG\\SQLEXPRESS;initial catalog=QuanLyThuVienDB;integrated security=True;trustservercertificate=True;MultipleActiveResultSets=True;App=EntityFramework";
+
+        string imagePath = ""; // Lưu đường dẫn ảnh tạm thời
+
+        public frmQLNhanVien()
         {
             InitializeComponent();
+        }
+
+        // Khi form được tải, hiển thị dữ liệu
+        private void frmQLNhanVien_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        // ------------------ HÀM LOAD DỮ LIỆU ------------------
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM NhanVien"; // Lấy tất cả nhân viên
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvNV.DataSource = dt;
+            }
+        }
+
+        // ------------------ NÚT THÊM (ĐĂNG KÝ) ------------------
+        private void btnDK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNV.Text))
+            {
+                MessageBox.Show("Vui lòng nhập mã nhân viên!");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    // Kiểm tra trùng mã
+                    string checkQuery = "SELECT COUNT(*) FROM NhanVien WHERE MaNhanVien = @MaNhanVien";
+                    SqlCommand checkCmd = new SqlCommand(checkQuery, conn);
+                    checkCmd.Parameters.AddWithValue("@MaNhanVien", txtMaNV.Text.Trim());
+                    int exists = (int)checkCmd.ExecuteScalar();
+
+                    if (exists > 0)
+                    {
+                        MessageBox.Show("Mã nhân viên đã tồn tại!");
+                        return;
+                    }
+
+                    // Thêm nhân viên mới
+                    string insertQuery = @"INSERT INTO NhanVien (MaNhanVien, TenNhanVien, SoDienThoai, DiaChi, GioiTinh, MatKhau)
+                                           VALUES (@MaNhanVien, @TenNhanVien, @SoDienThoai, @DiaChi, @GioiTinh, @MatKhau)";
+                    SqlCommand cmd = new SqlCommand(insertQuery, conn);
+                    cmd.Parameters.AddWithValue("@MaNhanVien", txtMaNV.Text.Trim());
+                    cmd.Parameters.AddWithValue("@TenNhanVien", txtTenNV.Text.Trim());
+                    cmd.Parameters.AddWithValue("@SoDienThoai", txtSDT.Text.Trim());
+                    cmd.Parameters.AddWithValue("@DiaChi", txtDiaChi.Text.Trim());
+                    cmd.Parameters.AddWithValue("@GioiTinh", rdNam.Checked ? "Nam" : "Nữ");
+                    cmd.Parameters.AddWithValue("@MatKhau", txtMK.Text.Trim());
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Thêm nhân viên mới thành công!");
+                }
+
+                LoadData();
+                ClearInput();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi thêm nhân viên: " + ex.Message);
+            }
+        }
+
+        // ------------------ NÚT SỬA (ĐỔI MẬT KHẨU) ------------------
+        private void btnDoiMK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNV.Text))
+            {
+                MessageBox.Show("Vui lòng nhập mã nhân viên cần sửa!");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string updateQuery = @"UPDATE NhanVien
+                                           SET TenNhanVien = @TenNhanVien, 
+                                               SoDienThoai = @SoDienThoai, 
+                                               DiaChi = @DiaChi,
+                                               GioiTinh = @GioiTinh, 
+                                               MatKhau = @MatKhau
+                                           WHERE MaNhanVien = @MaNhanVien";
+                    SqlCommand cmd = new SqlCommand(updateQuery, conn);
+                    cmd.Parameters.AddWithValue("@MaNhanVien", txtMaNV.Text.Trim());
+                    cmd.Parameters.AddWithValue("@TenNhanVien", txtTenNV.Text.Trim());
+                    cmd.Parameters.AddWithValue("@SoDienThoai", txtSDT.Text.Trim());
+                    cmd.Parameters.AddWithValue("@DiaChi", txtDiaChi.Text.Trim());
+                    cmd.Parameters.AddWithValue("@GioiTinh", rdNam.Checked ? "Nam" : "Nữ");
+                    cmd.Parameters.AddWithValue("@MatKhau", txtMK.Text.Trim());
+
+                    conn.Open();
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                        MessageBox.Show("Cập nhật nhân viên thành công!");
+                    else
+                        MessageBox.Show("Không tìm thấy nhân viên để cập nhật!");
+                }
+
+                LoadData();
+                ClearInput();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi cập nhật: " + ex.Message);
+            }
+        }
+
+        // ------------------ NÚT XÓA ------------------
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNV.Text))
+            {
+                MessageBox.Show("Vui lòng chọn nhân viên cần xóa!");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Bạn có chắc muốn xóa nhân viên này?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.No)
+                return;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM NhanVien WHERE MaNhanVien = @MaNhanVien";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@MaNhanVien", txtMaNV.Text.Trim());
+                    conn.Open();
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                        MessageBox.Show("Xóa nhân viên thành công!");
+                    else
+                        MessageBox.Show("Không tìm thấy nhân viên cần xóa!");
+                }
+
+                LoadData();
+                ClearInput();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi xóa nhân viên: " + ex.Message);
+            }
+        }
+
+        // ------------------ NÚT THOÁT ------------------
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // ------------------ CHỌN DÒNG TRONG DATAGRIDVIEW ------------------
+        private void dgvNV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvNV.Rows[e.RowIndex];
+                txtMaNV.Text = row.Cells["MaNhanVien"].Value?.ToString();
+                txtTenNV.Text = row.Cells["TenNhanVien"].Value?.ToString();
+                txtSDT.Text = row.Cells["SoDienThoai"].Value?.ToString();
+                txtDiaChi.Text = row.Cells["DiaChi"].Value?.ToString();
+                txtMK.Text = row.Cells["MatKhau"].Value?.ToString();
+
+                string gioiTinh = row.Cells["GioiTinh"].Value?.ToString();
+                if (gioiTinh == "Nam") rdNam.Checked = true;
+                else rdNu.Checked = true;
+            }
+        }
+
+        // ------------------ HÀM XÓA DỮ LIỆU TRÊN FORM ------------------
+        private void ClearInput()
+        {
+            txtMaNV.Clear();
+            txtTenNV.Clear();
+            txtSDT.Clear();
+            txtDiaChi.Clear();
+            txtMK.Clear();
+            rdNam.Checked = false;
+            rdNu.Checked = false;
         }
     }
 }
